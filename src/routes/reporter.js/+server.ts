df8fa04ef2dc148ef894b9169562a7fd10d556fd
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2026-01-29 21:43:22 +0100

    ✨ Add feature to disable adding current version to comment in /reporter.js

--- src/routes/reporter.js/+server.ts
+++ src/routes/reporter.js/+server.ts
@@ -1,14 +1,16 @@
 import reporterScript from '$lib/reporter?raw';
 import { text } from '@sveltejs/kit';
 
-export async function GET() {
+export async function GET({ url }) {
 	const script = [
 		'/* eslint-disable */',
 		'/* oxlint-disable */',
 		'// @ts-nocheck',
 		'',
 		`// This script is served by Pleye, on /reporter.js of your instance.`,
-		`// Pleye version ${process.env.VERSION ?? "(unknown)"}`,
+		url.searchParams.has('no-version')
+			? ''
+			: `// Pleye version ${process.env.VERSION ?? '(unknown)'}`,
 		'',
 		'',
 		reporterScript
