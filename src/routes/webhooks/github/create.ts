7bddf90c2d425a25fdd952e94765f6975b7739ee
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2026-02-02 01:36:36 +0100

    🐛 Fix github webhook create

--- src/routes/webhooks/github/create.ts
+++ src/routes/webhooks/github/create.ts
@@ -1,14 +1,12 @@
 import { db } from '$lib/server/db';
-import { branches } from '$lib/server/db/schema';
+import { branches, repositories } from '$lib/server/db/schema';
 import { error, json } from '@sveltejs/kit';
 import { type } from 'arktype';
 import { and, eq } from 'drizzle-orm';
 
 export const CreateEvent = type({
 	ref_type: '"tag" | "branch"',
-	ref: {
-		ref: 'string'
-	},
+	ref: 'string',
 	repository: {
 		id: 'number'
 	}
@@ -29,20 +27,23 @@ export async function onCreate(payload: unknown) {
 		return json({ ok: 'ignored' });
 	}
 
-	if (!data.ref.ref.startsWith('refs/heads/')) {
-		error(400, 'Invalid create payload: ref is not a branch');
-	}
-
-	const name = data.ref.ref.replace('refs/heads/', '');
-
 	const existing = await db.query.branches.findFirst({
-		where: and(eq(branches.repositoryId, data.repository.id), eq(branches.name, name))
+		where: and(eq(branches.repositoryId, data.repository.id), eq(branches.name, data.ref))
 	});
+
+	const repository = await db.query.repositories.findFirst({
+		where: eq(repositories.githubId, data.repository.id),
+		columns: { id: true }
+	});
+
+	if (!repository) {
+		error(400, 'Invalid create payload: unknown repository');
+	}
 
 	if (!existing) {
 		await db.insert(branches).values({
-			repositoryId: data.repository.id,
-			name
+			repositoryId: repository.id,
+			name: data.ref
 		});
 	}
 
