63b6a150db0b55492ef456355496ab8be5924174
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2026-07-19 11:06:44 +0200

    ✨ Support IDEs other than vscode for opening file location links

--- src/lib/filepaths.ts
+++ src/lib/filepaths.ts
@@ -10,6 +10,24 @@ export function vscodeURL({
 	if (!browser) return '';
 	const repositoryUserRoot = localStorage.getItem('repositoryUserRoot') || '';
 	if (!repositoryUserRoot) return '';
+	const fullpath = `${repositoryUserRoot}/${filePath}:${locationInFile.join(':')}`;
 
-	return `vscode://file/${repositoryUserRoot}/${filePath}:${locationInFile.join(':')}`;
+	const ide = localStorage.getItem('ide') || 'vscode';
+
+	switch (ide) {
+		case 'zed':
+			return `zed://file/${fullpath}`;
+		case 'vscode':
+			return `vscode://file/${fullpath}`;
+		case 'webstorm':
+			return `webstorm://open?${new URLSearchParams({
+				file: `${repositoryUserRoot}/${filePath}`,
+				line: locationInFile[0].toString(),
+				column: locationInFile[1].toString()
+			})}`;
+		default:
+			console.error(`Invalid IDE ${ide}. Supported IDEs are: "zed", "vscode" and "webstorm".`);
+	}
+
+	return `#invalid-ide-${ide}-supported-are-zed-vscode-webstorm`;
 }
